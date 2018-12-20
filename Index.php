b33401d80fbe8b51277bc6e943536d56d4741abd
rene4561 <44805521+rene4561@users.noreply.github.com>
2018-12-20 13:36:33 +0100

    Indsat href på alle overemner
    Har også indsat billeder til nogle underemner

--- Index.php
+++ Index.php
@@ -28,70 +28,82 @@
                 </a>
             </section>
             <section id="design">
-                <div class="emnecontainer">
-                    <div class="thumbcontainer">
-                        <img src="Billeder/design.png" alt="Abstrakt geometrisk figur">
+                <a href="Design/design.php">
+                    <div class="emnecontainer">
+                        <div class="thumbcontainer">
+                            <img src="Billeder/design.png" alt="Abstrakt geometrisk figur">
+                        </div>
+                        <div class="tekstcontainer">
+                            <h2>Design</h2>
+                            <p>Læs om grafisk design af webindhold.</p>
+                        </div>
                     </div>
-                    <div class="tekstcontainer">
-                        <h2>Design</h2>
-                        <p>Læs om grafisk design af webindhold.</p>
-                    </div>
-                </div>
+                </a>
             </section>
             <section id="interface">
-                 <div class="emnecontainer">
-                     <div class="thumbcontainer">
-                        <img src="Billeder/interface.jpg" alt="Illustration af mobiltelefon">
-                     </div>
-                    <div class="tekstcontainer">
-                        <h2>Interface design</h2>
-                        <p>Interface er kontanken mellem bruger og maskine. Giv dem en god opvelelse.</p>
+                <a href="Interface/interface.php">
+                     <div class="emnecontainer">
+                         <div class="thumbcontainer">
+                            <img src="Billeder/interface.jpg" alt="Illustration af mobiltelefon">
+                         </div>
+                        <div class="tekstcontainer">
+                            <h2>Interface design</h2>
+                            <p>Interface er kontanken mellem bruger og maskine. Giv dem en god opvelelse.</p>
+                        </div>
                     </div>
-                </div>
+                </a>
             </section>
             <section id="kommunikation">
-                <div class="emnecontainer">
-                    <div class="thumbcontainer">
-                        <img src="Billeder/kommunikation.jpg" alt="Illustation af 2 mennesker der kommunikerer">
-                     </div>
-                     <div class="tekstcontainer">
-                        <h2>Kommunikation</h2>   
-                        <p>Læs om kommunikation på forskellige platforme, samt videnskabsteori.</p>
+                <a href="Kommunikation/kommunikation.php">
+                    <div class="emnecontainer">
+                        <div class="thumbcontainer">
+                            <img src="Billeder/kommunikation.jpg" alt="Illustation af 2 mennesker der kommunikerer">
+                         </div>
+                         <div class="tekstcontainer">
+                            <h2>Kommunikation</h2>   
+                            <p>Læs om kommunikation på forskellige platforme, samt videnskabsteori.</p>
+                        </div>
                     </div>
-                </div>
+                </a>
             </section>
             <section id="virksomhed">
-                <div class="emnecontainer">
-                    <div class="thumbcontainer">
-                        <img src="Billeder/virksomhedsforst%C3%A5else.png" alt="Liste af forskellige virksomhedstyper">
+                <a href="Virksomhedsforst%25C3%25A5else/virksomhed.php">
+                    <div class="emnecontainer">
+                        <div class="thumbcontainer">
+                            <img src="Billeder/virksomhedsforst%C3%A5else.png" alt="Liste af forskellige virksomhedstyper">
+                        </div>
+                        <div class="tekstcontainer">
+                            <h2>Virksomhedsforståelse</h2>
+                            <p>Læs om virksmhedsforståelse - herunder Business Model Canvas.</p>
+                        </div>
                     </div>
-                    <div class="tekstcontainer">
-                        <h2>Virksomhedsforståelse</h2>
-                        <p>Læs om virksmhedsforståelse - herunder Business Model Canvas.</p>
-                    </div>
-                </div>
+                </a>
             </section>
             <section id="projekter">
-                <div class="emnecontainer">
-                    <div class="thumbcontainer">
-                        <img src="Billeder/design%20thinking.png" alt="Design thinking prossesen">                
+                <a href="Projekter/projekter.php">
+                    <div class="emnecontainer">
+                        <div class="thumbcontainer">
+                            <img src="Billeder/design%20thinking.png" alt="Design thinking prossesen">                
+                        </div>
+                        <div class="tekstcontainer">
+                            <h2>Projekter</h2>
+                            <p>Her kan du læse om de projekter vi har lavet - dertil de metoder vi har anvendt.</p>
+                        </div>
                     </div>
-                    <div class="tekstcontainer">
-                        <h2>Projekter</h2>
-                        <p>Her kan du læse om de projekter vi har lavet - dertil de metoder vi har anvendt.</p>
-                    </div>
-                 </div>
+                </a>
             </section>   
             <section id="omsiden">
-                <div class="emnecontainer">
-                    <div class="thumbcontainer">
-                        <img src="Billeder/Mig.jpg" alt="Et billede af René">
+                <a href="Om%2520siden/omsiden.php">
+                    <div class="emnecontainer">
+                        <div class="thumbcontainer">
+                            <img src="Billeder/Mig.jpg" alt="Et billede af René">
+                        </div>
+                        <div class="tekstcontainer">
+                            <h2>Om siden</h2>
+                            <p>Få indblik i produktionen bag sitet, samt de overvejelser jeg har gjordt mig undervejs.</p>
+                        </div>
                     </div>
-                    <div class="tekstcontainer">
-                        <h2>Om siden</h2>
-                        <p>Få indblik i produktionen bag sitet, samt de overvejelser jeg har gjordt mig undervejs.</p>
-                    </div>
-                </div>
+                </a>
             </section>
         </div>
         <footer>

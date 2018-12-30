8030b83d77684837ec74b68190502d11a3c848c8
rene4561 <44805521+rene4561@users.noreply.github.com>
2018-12-30 15:34:28 +0100

    indsat tekster
    SEO og webperioder

---
+++ Webudvikling/SEO.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Forside til mit eksamensprojekt</title>
+        <link rel="stylesheet" type="text/css" href="../mystyle.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="../mynav.js"></script>  
+    </head>
+    <body>
+        <?php
+            include "../minnav.include"
+        ?>
+        <div id=pagewrap>
+            <section id="indhold">
+                <div id="oversigt">
+                    <h1>SEO</h1>
+                    <p>SEO, Seach Engine Optimization, kan man betragte som gratis markedsføring. Ved at optimerer sin hjemmeside efter søgemaskinerne vil man, hvis det er gjort godt, blive rangeret over sine konkurrenter, når der søges efter et produkt eller lign. Dette er netop den væsentligste forskel fra andet markedsføring. For når man laver SEO, er det kunden som henvender sig til ens hjemmeside. Der findes 3 overordnede kategorier, når man skal lave SEO – SEO på sideniveau, link building og nogle tekniske barrierer.</p>
+                </div>
+                <section id="underemne1">
+                    <section>
+                        <p>SEO på sideniveau er kendetegnet ved, at man arbejder med sin tekstforfatning og sin HTML strukturering. Når man skriver sin webtekst, skal man helst som minimum skrive 300-400 ord – af dem burde 3-5% være søgeord. Dette har den betydning at Google ser, at siden indeholder mange af de søgeord som brugeren leder efter. Dertil kan man med fordel benytte sig af synonymer, da man aldrig kan forudse, hvad brugeren vil kalde det de leder efter. OBS – det er vigtigt at man ikke kun skriver tekst til søgemaskinen, man skal også huske brugeren.</p>
+                        <p>Det er også vigtigt, og relativt nemt, at inddele sin tekst korrekt i HTML. Det der menes med dette er, at man anvender semantiske tags og nesting. Disse hjælper Google med at navigerer rundt i indholdet og udvælge hvad der er vigtigt og knapt så vigtigt, samt hvad der er relateret til hvad.</p>
+                    </section>
+                    <section>
+                        <p>Linkbuilding dækker over, at man skaber forbindelser både internt på siden, såvel som eksternt. Disse findes i overordnede typer. </p>
+                        <p>Eksterne links er dem, hvor andre hjemmesider linker til din side.</p>
+                        <p>Intern linkbuilding dækker over den interne struktur, med links til andre dokumenter på samme hjemmeside.</p>
+                        <p>Sidst findes der udegående links, og disse handler om at hjælpe kunden fra din hjemmeside til andre relevante hjemmesider. Alt dette vil sørge for, at Google vælger din hjemmeside. Man kan tænkte på det, som en by med god vejinfrastruktur. For god vejinfrastruktur gør at bileren kan komme, hvorhen de vil. </p>
+                    </section>
+                    <section>
+                        <p>De tekniske barriere kan være ting som videoer, kopieret interntekst og load hastigheder. Videoer kan være problematiske af 2 årsager: for det første siger videoer ikke noget om dens indhold - for en søgemaskine som Google. Dermed får man ingen SEO fra dem. For det andet kan videofiler også fylde meget på siden. Dertil vil siden loade langsommere, og dermed vil Google nedrangere siden.</p>
+                    </section>
+                </section>
+                <aside id="pause1">
+                    <a href="#">
+                        <div class="pause">
+                            <div class="pausetekst">
+                                <h2>Noget releteret til emnet foroven</h2>
+                                <p>Læs om det eller andet mega sejt reletert emne eller værktøj. Klik til et ekstern eller intern link.</p>    
+                            </div>
+                            <div class="pauseimg">
+                                <img src="thumbnails/test.png">
+                            </div>
+                        </div> <!--pause1 slut -->
+                    </a>
+              </aside> 
+            </section> <!--indhold slut-->
+        </div>
+        <footer>
+            <div id="footerkontakt">
+            <h2>Kontakt</h2>
+                <p>Email: rene4561@student.eadania.dk</p>
+            </div>
+            <div id=footerinfo>
+                <p>Denne side er udarbejdet som et eksamensprojekt af René Støvlbæk Krog - eadania Skive, MMD 2018</p>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ Webudvikling/Webbetsperioder.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Responsiv layout</title>
+        <link rel="stylesheet" type="text/css" href="../mystyle.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="../mynav.js"></script>  
+    </head>
+    <body>
+        <?php
+            include "../minnav.include"
+        ?>
+        <div id=pagewrap>
+            <section id="indhold">
+                <div id="oversigt">
+                    <h1>Webbets perioder</h1>
+                    <p>Her kan man læse om de perioder som webbet har gennemgået, siden det blev tilgængeligt for offentligheden. Hovedpointen her er, at webbet forandrer sig hele tiden. Nye opfindelser og koncepter kommer til, og det skal vi, som multimediedesignere, tilpasse os efter.</p>
+                    <blockquote>”Vær’ nysgerrige på at tilegne jer ny viden” - Niels Østergaard, under en undervisningsgang. </blockquote>
+                    <ul>
+                        <li>Tidelige eksperimenter</li>
+                        <li>Under konstruktion</li>
+                        <li>Semantsik søgning</li>
+                        <li>Sociale netværk</li>
+                        <li>Det mobile web</li>
+                    </ul>
+                </div>
+                <section id="underemne1">
+                    <h2>Tidelige eksperimenter (fra 1993)</h2>
+                    <p>Denne periode er kendetegnet ved at alle eksperimenterede med, hvordan en hjemmeside skulle se ud og struktureres. Dette skyldes blandt andet, at det også var året hvor Mosaic blev lanceret – dette var den første browser med grafik. Der var mange begrænsninger i HTML-sproget. Derudover var det også svært at forudse hvordan browserne fortolkede siderne, da der ikke var nogen givne standarder. Dengang kunstnere fik øjnene op for den nye platform, begyndte de også at komme med bud på, hvordan man kunne bruge hjemmesider til blandt andet visuel fortælling.</p>
+                    <img src="Modeller/perioder_whitehouse.png" alt="Et screenshot at det hvide huses hjemmeside under perioden">
+                </section>
+                <section id="underemne2">
+                    <h2>Under konstruktion (fra 1996)</h2>
+                    <p>Under denne periode tilsluttede mange private virksomheder sig dillen - om at få sin egen hjemmeside. Desværre var der mange, som gik i stå under udviklingen af deres hjemmeside. Dette resulterede i at mange hjemmesider fik indsat en gif, med en mand der graver, hvor teksten ”under konstruktion” stod forneden. </p>
+                    <section>
+                        <h2>Guruernes kamp</h2>
+                        <p>Nu hvor internettet havde nået ud til pøblen, var der mange professionelle designere som også kom med deres bud på webbets muligheder. David Seiegel blev fortaler for at man skulle designe hjemmesider visuelt. Han anvendte dertil en visuel metafor for at fremme meningen med hjemmesiden. </p>
+                        <img src="Modeller/perioder_guru.png" alt="Jimtown stores hjemme, under perioden, skabt af David Seiegel">
+                        <p>I den anden ende af dette spektrum var danskeren Jakob Nielsen, ph.d i usabillity engeneering,  som var fortaler for funktionalitet over design. Med sin baggrund skrev han en bog, hvori han belyste vigtigheden af brugervenlighed.</p>
+                        <img src="Modeller/perioder_nielsen.png" alt="Jakob Nielsens hjemmeside under perioden">
+                    </section>
+                </section>
+                <section id="underemne3">
+                    <h2>Semantisk søgning (fra 2000)</h2>
+                    <p>Det var i denne periode, hvor søgemaskinen Google opstod. Det der gjorde Google til noget specielt var, at den rent faktisk kunne finde det, som brugeren søgte på. Siden anvendte en algoritme, som var i stand til at bedømme sidens indhold og betydning. Efter dette begyndte man at anvende SEO - Search Engine Optimization. </p>
+                    <p>Det var også under denne periode, at man begyndte at tænke på at separere struktur og design. Deraf udviklede man CSS - Cascading Style Sheets. </p>
+                </section>
+                <section id="underemne4">
+                    <h2>Det sociale nætværk (fra 2003)</h2>
+                    <p>Under denne periode indså man at magten over internettet ikke længere lå hos de etablerede autoriteter, men hos brugerne. For at fastslå dette navngav man det nye web som web 2.0. Et nyt begreb som kom til syne, var konceptet om den lange hale.</p>
+                    <img src="Modeller/perioder_denlangehale.png" alt="Modellen over den lange hale">
+                    <p>Den lange hale blev først italesat af Chris Anderson. Webbet har den særlige egenskab at der, i teorien, findes uendelig lagerplads. Normale butikker har haft begrænset lagerplads, og har derfor skulle sælge noget, som de vidste at de kunne sælge. På webbet er forholdene dog ikke således. Halen er derfor en række nicheprodukter- og ydelser, som tilsammen udgør en langt større del af markedet samlet set.</p>
+                    <p>Myspace kom også til under denne periode, og dette var starten på de sociale medier. Senere hen overtog Facebook markedet. Youtube, twitter m.fl. kom også frem i lyset. </p>
+                </section>
+                <section id="underemne5">
+                    <h2>Det mobile web (fra 2010)</h2>
+                    <p>Da iPhone kom til i 2007 og iPad’en i 2010, begyndte det mobile web at tage form. Alle disse nye smartphones og tablets, som begyndte at strømme ud på markedet, fik den betydning, at man nu skulle tilpasse sin hjemmeside således, at den virkede optimalt på alle skærmstørrelser.</p>
+                </section>
+                <aside id="pause1">
+                    <a href="#">
+                        <div class="pause">
+                            <div class="pausetekst">
+                                <h2>Noget releteret til emnet foroven</h2>
+                                <p>Læs om det eller andet mega sejt reletert emne eller værktøj. Klik til et ekstern eller intern link.</p>    
+                            </div>
+                            <div class="pauseimg">
+                                <img src="thumbnails/test.png">
+                            </div>
+                        </div> <!--pause1 slut -->
+                    </a>
+              </aside> 
+            </section> <!--indhold slut-->
+        </div>
+        <footer>
+            <div id="footerkontakt">
+            <h2>Kontakt</h2>
+                <p>Email: rene4561@student.eadania.dk</p>
+            </div>
+            <div id=footerinfo>
+                <p>Denne side er udarbejdet som et eksamensprojekt af René Støvlbæk Krog - eadania Skive, MMD 2018</p>
+            </div>
+        </footer>
+    </body>
+</html>

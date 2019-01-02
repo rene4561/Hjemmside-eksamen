b7386d11c5005012e6b0142acb5345c70f0ae019
rene4561 <44805521+rene4561@users.noreply.github.com>
2019-01-02 18:14:32 +0100

    Indsat flere sider
    videnskabs teori, præsentationsteknik og design thinking

---
+++ Kommunikation/Præsentationsteknik.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="da">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Forside til mit eksamensprojekt</title>
+        <link rel="stylesheet" type="text/css" href="../mystyle.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Glegoo|Ubuntu:300" rel="stylesheet">
+        <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+        <script src="../mynav.js"></script>  
+    </head>
+    <body>
+        <?php
+            include "../minnav.include"
+        ?>
+        <div id=pagewrap>
+            <section id="indhold">
+                <div id="oversigt">
+                    <h1>Præsentationsteknik</h1>
+                    <p>Når vi som multimediedesignere skal præsentere vores produkter, til jobsamtaler, møder eller på studiet skal præsentere en fremlæggelse på klassen, så er det vigtigt at man kender til kommunikationen blandt mennesker. Store dele af dette afsnit er udarbejdet ud fra kapitel 4 og 6 fra bogen, Mette Hald - Præsentation og formidling.</p>
+                    <p>Under denne sektion kan du læse om følgende emner:</p>
+                    <ul>
+                        <li>Non-verbal kommunikation - herunder kropssprog</li>
+                        <li>Præsentationsforberedelse</li>
+                        <li>
+                            <ul>
+                                <li>Fase 1 - find soffet</li>
+                                <li>Fase 2 - disponer stoffet</li>
+                                <li>Formuler stoffet</li>
+                                <li>Fase 4 - øvelse</li>
+                                <li>Fase 5 - præsentationen</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <section id="underemne1">
+                    <h2>Non-verbal kommunikation</h2>
+                    <p>Når vi kommunikerer med andre mennesker, så kan vi kommunikere igennem vores kropssprog, det nonverbale, og gennem vores sprog som er det verbale. </p>
+                    <p>Der findes mange måder at kommunikerer non-verbalt, dette omfatter: Kropssprog, herunder holdning, stemmeleje - herunder; betoning af det man bruger, vejrtrækning og taletempo. Ansigtsudtryk, øjenbevægelser ,gestus/fagter, håndtryk, afstand til andre og påklædning. </p>
+                    <p>Kropssprog kan afsløre meget om ens personlighed, alt efter hvor bevidst man er om sit kropssprog. Man kan sige, at der findes nogle hovedtræk ved kropssproget, de står listet nedenfor: </p>
+                    <ul>
+                        <li>Passivt kropssprog: Hvis man står meget sammenfaldende med hænderne i lommerne eller bruger små skridt - alt dette giver indtryk af, at man er meget uengageret eller usikker.</li>
+                        <li>Aktivt kropssprog: Modsat passivt kropssprog findes aktivt kropssprog - altså kontrol og energi. Dette sker gennem en rank holdning, fast fodfæste og levende øjne.</li>
+                        <li>Skuldrene afslører meget: Hævede skuldre viser anspændthed, hvor sænkede skuldre viser dermed det modsatte.</li>
+                        <li>Frustration: Der findes mange måder, hvorpå kroppen udtrykker frustration som blandt andet hænderne, vejrtrækning, blik, stemmen og sine bevægelser.</li>
+                    </ul>
+                    <p>Når man skal præsentere sig selv til en jobsamtale, til en præsentation på skolen, til et møde og lignende, ville det være mest hensigtsmæssigt at have et åbent og aktiv kropssprog. Det ses i form af, at man er smilende, rolig og afslappet. Man virker interesseret, og har et fokuseret blik.</p>
+                    <p>Man skal trække vejret, have kontrol over sig selv, fylde rummet ud på en positiv måde og udstråle at man har styr på sin præsentation.</p>
+                </section>
+                <section id="underemne2">
+                    <h2>Forberedelsen til den gode præsentation</h2>
+                    <p>Før man præsenterer et emne, vil det være en god ide at stille disse to basale spørgsmål:</p>
+                    <p>Hvad skal jeg formidle, hvad er væsentligt, hvad skal tilhørerene have ud af præsentationen? </p>
+                    <p>Hvem skal jeg præsentere for?</p>
+                    <p>Herefter skal man arbejde med sin præsentation. Det kan man gøre vha. nedenstående 5 faser: </p>
+                    <img src="Modeller/Pr%C3%A6sentation_5faser.PNG" alt="Overblik over de 5 fase - fint stoffet, disponer stoffet, formunler stoffet, øvelse og præsentationen">
+                    <p>De 5 ovenstående faser kan deles op i grupper: de 4 første faser beskæftiger sig med præsentationsforberedelsen, hvorimod den anden beskæftiger sig med selve præsentationen.</p>
+                    <section> <!-- Fase 1 start -->
+                        <h2>Arbejdsfase 1: Find stoffet</h2>
+                        <p>I denne fase skal man fokusere på, hvad der er det vigtigste i emnet. For at finde frem til hvad der er det mest relevante, kan man inddele denne fase i 3 underfaser: </p>
+                        <p>Overbliksfasen: Her skal man danne sig et klar billede af, hvad præsentationen skal handle om i store træk. </p>
+                        <p>Udvælgelsesfasen: Her skal man udvælge de mest væsentlige punkter man har fundet, altså hvad skal med, og hvad skal ikke med? Hvordan skabes der struktur over emnet, det lytteren får størst udbytte af præsentationen?</p>
+                        <p>Fordybelsesfasen: Denne fase begynder, når man har udvalgt det stof, der skal indgå i sin præsentation. Man skal finde den 'røde tråd' i præsentationen. Dertil skal man finde sit fokuspunkt. Man skal fravælge unødvendigt materiale fra - kill your darlings.</p>
+                    </section> <!-- Fase 1 slut -->
+                    <section> <!-- Fase 2 start -->
+                        <h2>Arbejdsfase 2: Disponer stoffet </h2>
+                        <section> <!-- Heksagon start -->
+                            <p>Når man har udvalg sit stof og de kilder man vil anvende, så skal man være bevidst, hvad man arbejder hen imod. Man kan anvende det retoriske heksagon, til at udarbejde sin disposition med.</p>
+                            <img src="Modeller/pr%C3%A6sentation_heksagon.png" alt="Kommunikationsheksagon">
+                            <p>I heksagonnet ses talen i midten. Herudfra trækkes der streger ud til hvert felt, hvor alle disse er forbundne på kryds og tværs.</p>
+                            <p>I afsenderfeltet handler det om at være bevidst om sig selv, hvordan man kommunikere - både verbalt og nonverbalt. Dertil man skal være bevidst om, hvem man taler til - altså modtageren.</p>
+                            <p>Mellem disse to er der et emne, som skal formidles fra afsender til modtager, hvor talen - altså præsentationen er mediet.  Derfra har begge parter også et formål med kommunikationen.</p>
+                            <p>Situationen kan betragtes som omstændighederne, som præsentationen forgår under. Der er flere elementer i spil under situationen. Der er er fysiske, mentale og sociale rammer for kommunikationssituationen. </p>
+                            <p>Efter overvejelse af situationen kan man derefter overveje, hvilket sprog der vil mere mest hensigtsmæssig at benytte sig af: formel/uformel, faglig/ikke-faglig mm.</p>
+                            <p>- PDF ”Present 3 pp. 27-44”</p>
+                        </section> <!-- Heksagon slut -->
+                        <section> <!-- Fisk start -->
+                            <p>Derudover skal man skabe en struktur over sit materiale og emnet. Det er med til at gøre arbejdet mere overskueligt, da man har et klart mål man arbejder sig imod. Man inddeler ofte sit stof i 3 dele, se nedenstående figur:</p>
+                            <img src="Modeller/pr%C3%A6sentation_fisk.png" alt="Fisken, en model der inddeler en præsentation i 3 faser">
+                            <p>Modellen er inddelt i en indledning, hoveddel og en afslutning:</p>
+                            <p>Indledning: Denne del har stor betydning for, hvordan din modtager danner sit billede af, hvem man er: "You never get a second chance to make a first impression". Det er altså her, hvor man skal fange modtageren. Man kan evt. bruge humor, sige noget provokerende eller fortælle en sjov anekdote om sig selv. Man kan gøre meget, men man skal blot kunne stå indenfor det.</p>
+                            <p>Man kan bruge disse tre punkter, som en tjekliste til en god indledning, hvor man skal få tilhørende til at opnå:</p>
+                            <section>
+                                <p>Velvilje - de skal være positive overfor det, som man skal præsentere.</p>
+                                <p>Opmærksomhed - tilhørende skal gøres interesserede i det, du skal præsentere.</p>
+                                <p>Lærevillige - de skal ønske at lære noget af det, man præsenterer for dem.</p>
+                            </section>
+                        <p>Hovedel: Her skal stoffet præsenteres, og her ligger ca. 80% af præsentationen.</p> 
+                        <p>Det er en god idé at præsentere sin disposition, for så ved lytteren hvad man kan forvente af din præstation og rækkefølgen. Hvis man skal begrunde sit valg af emnet, er det vigtigt at man begrænser sine argumenter til 2-3 stykker. Det andet kan virke forvirrende, hvor de enkelte begrænsede argumenter viser, at man har ro og overblik over sit emne.</p>
+                        <p>Afslutning: Afslutningen er ligeså vigtig som indledningen, da det er her man sætter punktum for sin fremlæggelse. Den skal være kort og præcis, og man skal vise at man har nået det, der var intentionen med præsentationen. Man kunne evt. opsummere hvert hovedpunkt, så man forstærker den røde tråd i fremlæggelsen.</p>
+                        </section><!-- Fisken slut -->
+                    </section> <!-- Fase 2 slut -->
+                    <section> <!-- Fase 3 start -->
+                        <h2>Arbejdsfase 3: Formuler din præsentation </h2>
+                        <p>Her skal man overveje, hvilket sprog man vil præsentere sit stof i. Her er ikke tale om modersmålet, men derimod hvordan man udtrykker sig. Man skal formulere sig motiveret og velformuleret over for de tilhørerene. Hertil er der 4 grundlæggende krav til sproget i en god præsentation: </p>
+                        <p>Korrekt - tal grammatisk korrekt, disse fejl kan medføre at lytteren tænker over talefejlene, fremfor indholdet. Dertil kan det skabe forvirringer. </p>
+                        <p>Klart - tal præcist om det man præsenterer. Det minimerer misforståelser, og man kommer til at virke sikker i stoffet. </p>
+                        <p>Tilpasset - man skal ramme modtagergruppen, så det betyder, at man skal tilpasse sproget efter modtageren.</p>
+                        <p>Smukt - sproget er ens værktøj, og jo bedre man behersker det, des større bliver nuancerne i sproget. Det betyder, at man bedre kan ramme sin målgruppe, og dermed nemmere flytte dem i den retning, som man gerne vil.</p>
+                    </section> <!-- Fase 3 slut -->
+                    <section> <!-- Fase 4 start -->
+                        <h2>Arbejdsfase 4: Forbered fremlæggelsen </h2>
+                        <p>Man kan frigøre sig fra manuskriptet til sin præsentation ved at øve sig på denne. Når man ikke er bundet til et stykke papir, virker man mere sikker. Dertil viser det også, at man er engageret i stoffet. Det kan smitte af på lytteren, da engagement ofte smitter. Derudover får man også lettere mulighed for at følge med i, hvordan lytteren reagere på fremlæggelsen. For så kan man nemmere kan ændre strategi, hvis lytteren virker uengageret. </p>
+                    </section> <!-- Fase 4 slut -->
+                    <section> <!-- Fase 5 start -->
+                        <h2>Arbejdsfase 5: Fremfør præstationen </h2>
+                        <section>
+                        <p>I det første sekund af sin præsentation, er det vigtigt at huske, at man er på fra starten af. Her skal man være bevidst om 3 ting: sig selv, sit kropssprog og sit stof. For er man er usikker på ét af disse punkter, så skaber det ubalance i præsentationen. </p>
+                        <p>Man kunne bruge denne checkliste, som en guide til sin præsentation: </p>
+                        <ul>
+                            <li>Hvor godt er jeg inde i det emne, jeg skal præsentere?</li>
+                            <li>Hvordan er min personlige fremtræden?</li>
+                            <li>Hvordan er mit kropssprog? </li>
+                            <li>Hvilke signaler sender det tøj jeg har på?</li>
+                            <li>Hvem er min målgruppe?</li>
+                            <li>Hvad er målet med præsentationen? </li>
+                            <li>Hvad skal modtagergruppen have ud af præsentationen?</li>
+                        </ul>
+                        </section>
+                        <section>
+                            <h2>Hvad kan man bruge de 5 faser til?</h2>
+                            <p>Overordnet skal man øve sig i at kunne præsentere et emne, og ovenstående 5 faser er en god vejledning til, hvordan man opnår en god præsentation - til f.eks. jobsamtalen, præsentationen på skolen eller i lignende situationer.</p>
+                        </section>
+                    </section> <!-- Fase 5 slut -->
+                </section> <!-- Under emne 2 slut -->
+                <aside id="pause1">
+                    <a href="#">
+                        <div class="pause">
+                            <div class="pausetekst">
+                                <h2>Noget releteret til emnet foroven</h2>
+                                <p>Læs om det eller andet mega sejt reletert emne eller værktøj. Klik til et ekstern eller intern link.</p>    
+                            </div>
+                            <div class="pauseimg">
+                                <img src="thumbnails/test.png">
+                            </div>
+                        </div> <!--pause1 slut -->
+                    </a>
+              </aside> 
+            </section> <!--indhold slut-->
+        </div>
+        <footer>
+            <div id="footerkontakt">
+            <h2>Kontakt</h2>
+                <p>Email: rene4561@student.eadania.dk</p>
+            </div>
+            <div id=footerinfo>
+                <p>Denne side er udarbejdet som et eksamensprojekt af René Støvlbæk Krog - eadania Skive, MMD 2018</p>
+            </div>
+        </footer>
+    </body>
+</html>

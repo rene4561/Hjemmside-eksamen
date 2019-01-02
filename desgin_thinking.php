1c2ede30b9f8af10096597d9a2084f6e4f92805b
rene4561 <44805521+rene4561@users.noreply.github.com>
2019-01-02 21:11:03 +0100

    Rettet fejl på DT

--- desgin_thinking.php
+++ desgin_thinking.php
@@ -65,18 +65,6 @@
                     <p>Under processen handler det, til dels, om at være nysgerrig og kreativ, men en vigtig del er også at være kritisk. Mens man er kreativ og nysgerrig, handler det om at udvide projektets rammer – at få én idé som kan lede til en anden osv. Dette kaldes at divergere. Dertil er det også ligeså vigtig at være kritisk og ’kill your darlings’ – her konvergerer man, hvilket betyder at man indsnævre projektets rammer igen. Denne divergens/konvergens proces bidrager med en masse løsninger, hvor kun de bedste tages med videre.</p>
                     <img src="Modeller/DT_divergentkonvergent.png" alt="Illustration over den divergente og konvergente arbejdsproces">
                 </section>
-                <section id="underemne3">
-                    <h2>Under emne 3</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae dignissim arcu. Curabitur condimentum aliquet odio, eu ornare neque ullamcorper pulvinar. Cras in dapibus odio. Aliquam eget orci sit amet lacus convallis pretium. Nulla in accumsan justo. Maecenas mattis est ac felis tincidunt imperdiet. Integer lacus diam, scelerisque et nibh ac, consequat maximus felis. Pellentesque mollis blandit dolor, ac bibendum tortor scelerisque ac. Nam imperdiet lobortis maximus. Sed luctus vehicula enim, quis commodo urna ullamcorper in. Etiam sollicitudin felis a risus viverra cursus. Integer ut mauris ullamcorper, auctor ante a, laoreet ex. Donec luctus non neque sit amet euismod.</p>
-                </section>
-                <section id="underemne4">
-                    <h2>Under emne 4</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae dignissim arcu. Curabitur condimentum aliquet odio, eu ornare neque ullamcorper pulvinar. Cras in dapibus odio. Aliquam eget orci sit amet lacus convallis pretium. Nulla in accumsan justo. Maecenas mattis est ac felis tincidunt imperdiet. Integer lacus diam, scelerisque et nibh ac, consequat maximus felis. Pellentesque mollis blandit dolor, ac bibendum tortor scelerisque ac. Nam imperdiet lobortis maximus. Sed luctus vehicula enim, quis commodo urna ullamcorper in. Etiam sollicitudin felis a risus viverra cursus. Integer ut mauris ullamcorper, auctor ante a, laoreet ex. Donec luctus non neque sit amet euismod.</p>
-                </section>
-                <section id="underemne5">
-                    <h2>Under emne 5</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae dignissim arcu. Curabitur condimentum aliquet odio, eu ornare neque ullamcorper pulvinar. Cras in dapibus odio. Aliquam eget orci sit amet lacus convallis pretium. Nulla in accumsan justo. Maecenas mattis est ac felis tincidunt imperdiet. Integer lacus diam, scelerisque et nibh ac, consequat maximus felis. Pellentesque mollis blandit dolor, ac bibendum tortor scelerisque ac. Nam imperdiet lobortis maximus. Sed luctus vehicula enim, quis commodo urna ullamcorper in. Etiam sollicitudin felis a risus viverra cursus. Integer ut mauris ullamcorper, auctor ante a, laoreet ex. Donec luctus non neque sit amet euismod.</p>
-                </section>
                 <aside id="pause1">
                     <a href="#">
                         <div class="pause">
